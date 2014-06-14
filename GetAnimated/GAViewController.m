62354fcf3bb2773b0802139dc810632aa5685c62
Joshua Howland <development@jkhowland.com>
2014-06-13 23:18:19 -0600

    Adds core animation code

--- GetAnimated/GAViewController.m
+++ GetAnimated/GAViewController.m
@@ -30,30 +30,94 @@
     smallBlock.backgroundColor = [UIColor yellowColor];
     [self.view addSubview:smallBlock];
     self.smallBlock = smallBlock;
-    
-    
-    CGAffineTransform scaleTrans = CGAffineTransformMakeScale(2, 2);
-    CGAffineTransform rotateTrans = CGAffineTransformMakeRotation(90 * M_PI / 180);
 
     
-    CGAffineTransform shrinkTrans = CGAffineTransformMakeScale(0.5, 0.5);
+    // Block animations
+
+//
+//    CGAffineTransform scaleTrans = CGAffineTransformMakeScale(2, 2);
+//    CGAffineTransform rotateTrans = CGAffineTransformMakeRotation(90 * M_PI / 180);
+//
+//    
+//    CGAffineTransform shrinkTrans = CGAffineTransformMakeScale(0.5, 0.5);
+//
+//    
+//    [UIView animateWithDuration:3.0 animations:^{
+//        self.block.alpha = 0.0;
+//        self.block.center = CGPointMake(self.block.center.x, self.block.center.y + 75);
+//        
+//        self.block.transform = CGAffineTransformConcat(scaleTrans, rotateTrans);
+//        
+//        self.smallBlock.alpha = 0.0;
+//        self.smallBlock.center = CGPointMake(self.block.center.x, self.block.center.y - 10);
+//
+//        self.smallBlock.transform = CGAffineTransformConcat(shrinkTrans, rotateTrans);
+//        
+//    } completion:^(BOOL finished) {
+//        self.block.alpha = 1.0;
+//        self.smallBlock.alpha = 1.0;
+//    }];
 
     
-    [UIView animateWithDuration:3.0 animations:^{
-        self.block.alpha = 0.0;
-        self.block.center = CGPointMake(self.block.center.x, self.block.center.y + 75);
-        
-        self.block.transform = CGAffineTransformConcat(scaleTrans, rotateTrans);
-        
-        self.smallBlock.alpha = 0.0;
-        self.smallBlock.center = CGPointMake(self.block.center.x, self.block.center.y - 10);
+    // Move animation
+    
+//    CABasicAnimation *animation = [CABasicAnimation animation];
+//    animation.keyPath = @"position.y";
+//    animation.fromValue = @(self.block.center.y);
+//    animation.toValue = @(self.block.center.y + 75);
+//    animation.duration = 3;
+//    
+//    [self.block.layer addAnimation:animation forKey:@"basic"];
+//    
+//    self.block.layer.position = CGPointMake(self.block.layer.position.x, self.block.layer.position.y + 75);
+    
 
-        self.smallBlock.transform = CGAffineTransformConcat(shrinkTrans, rotateTrans);
-        
-    } completion:^(BOOL finished) {
-        self.block.alpha = 1.0;
-        self.smallBlock.alpha = 1.0;
-    }];
+    
+    // Shake animation
+    
+//    CAKeyframeAnimation *keyFrameAnimation = [CAKeyframeAnimation animation];
+//    keyFrameAnimation.keyPath = @"position.x";
+//    keyFrameAnimation.values = @[ @0, @10, @-10, @10, @0 ];
+//    keyFrameAnimation.keyTimes = @[ @0, @(1 / 6.0), @(3 / 6.0), @(5 / 6.0), @1 ];
+//    keyFrameAnimation.duration = 1.0;
+//    
+//    keyFrameAnimation.additive = YES;
+//    
+//    [self.block.layer addAnimation:keyFrameAnimation forKey:@"shake"];
+//    [self.smallBlock.layer addAnimation:keyFrameAnimation forKey:@"shake"];
+    
+    
+
+    // Path animation
+    
+    
+//    CGRect boundingRect = CGRectMake(-150, -150, 300, 300);
+//    
+//    CAKeyframeAnimation *orbit = [CAKeyframeAnimation animation];
+//    orbit.keyPath = @"position";
+//    orbit.path = CFAutorelease(CGPathCreateWithEllipseInRect(boundingRect, NULL));
+//    orbit.duration = 4;
+//    orbit.additive = YES;
+//    orbit.repeatCount = HUGE_VALF;
+//    orbit.calculationMode = kCAAnimationPaced;
+//    orbit.rotationMode = kCAAnimationRotateAuto;
+//    
+//    [self.smallBlock.layer addAnimation:orbit forKey:@"orbit"];
+
+    
+    // Move animation (with timing)
+    
+//    CABasicAnimation *animation = [CABasicAnimation animation];
+//    animation.keyPath = @"position.y";
+//    animation.fromValue = @(self.block.center.y);
+//    animation.toValue = @(self.block.center.y + 175);
+//    animation.duration = 3;
+//    
+//    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+//
+//    [self.block.layer addAnimation:animation forKey:@"basic"];
+//
+//    self.block.layer.position = CGPointMake(self.block.layer.position.x, self.block.layer.position.y + 175);
 
     
 }
